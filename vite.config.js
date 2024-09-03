a314e386ea9145f48d01492d025406d8e4b44fce
Suhail-Shaik24 <shaikmohammed.suhail2021@vitstudent.ac.in>
2024-09-03 13:48:30 +0530

    commit#6

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Get-Your-Slots/',
-  build: {
-    outDir: 'build',
-  },
   plugins: [react()],
 })
