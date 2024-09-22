5a1c1e5b1e6d432d84d4e7bea5c0eb9c9ea6cb11
Suhail-Shaik24 <shaikmohammed.suhail2021@vitstudent.ac.in>
2024-09-22 15:30:21 +0530

    paymentTest#2

--- src/components/QRCodes/QRFreshersRealTime.jsx
+++ src/components/QRCodes/QRFreshersRealTime.jsx
@@ -11,7 +11,7 @@ const QRFreshersRealTime = ({ amount, verificationCode }) => {
     // GPay specific UPI deep link with Android intent
     const gpayLink = `intent://pay?pa=${UPI_ID}&pn=${NAME}&tn=Payment&am=1&cu=INR#Intent;scheme=upi;package=com.google.android.apps.nbu.paisa.user;end`;
     window.location.href = gpayLink;
-  }; 
+  };
 
 
   // const upiUrl = `upi://pay?pa=${UPI_ID}&pn=${NAME}&am=1&tn=${Transaction_Message}&cu=INR`;
@@ -19,13 +19,17 @@ const QRFreshersRealTime = ({ amount, verificationCode }) => {
   return (
     <div className="border-2 border-[#a3663c] w-full p-4 rounded-lg flex flex-col gap-3 justify-center items-center bg-gray-100 shadow-lg">
       <h2 className="text-xl font-bold font-['Open_Sans']font-semibold mb-2 text-center">Proceed to Pay <br /> <span className="text-4xl"> ₹{amount} </span></h2>
-      
-      <button onClick={handlePayment}>
-      <a href={upiUrl} target="_blank" className="font-bold text-white text-lg bg-[#2e358f] p-3 rounded lg:hidden"> Tap here to Pay</a></button>
 
-      <p className="lg:hidden">or Scan the QR Code</p>
+      <button onClick={handlePayment}>
+
+      <p className="font-bold text-white text-lg bg-[#2e358f] p-3 rounded lg:hidden"> Tap here to Pay</p>
+
+        {/* <a href={upiUrl} target="_blank" className="font-bold text-white text-lg bg-[#2e358f] p-3 rounded lg:hidden"> Tap here to Pay</a> */}
+      </button>
+
+      {/* <p className="lg:hidden">or Scan the QR Code</p>
       <h3 className="hidden text-lg font-semibold lg:block"> Scan the QR Code to make the Payment</h3>
-      <QRCodeSVG value={upiUrl} size={256} />
+      <QRCodeSVG value={upiUrl} size={256} /> */}
     </div>
   );
 };
